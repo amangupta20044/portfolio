58b469ce514f995957707954174a64bf8e393fe5
amangupta20044 <a.mann20044@gmail.com>
2026-06-12 10:30:25 +0530

    fix: update resume drive link to the correct URL

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,2 +1,2 @@
 export const RESUME_DRIVE_LINK =
-  "https://drive.google.com/file/d/YOUR_FILE_ID/view?usp=sharing";
+  "https://drive.google.com/file/d/1Gb6X3ct7GCTdmF57y8aV5Bkt9rAaiCZ5/view?usp=sharing";
